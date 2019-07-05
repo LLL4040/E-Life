9deed8ac3d1153d8a467ae01b9da6bb548948415
LLL4040 <2293114926@qq.com>
2019-07-05 17:08:47 +0800

    beautify the home page

--- code/e-life-web-app/src/components/index.vue
+++ code/e-life-web-app/src/components/index.vue
@@ -6,7 +6,7 @@
     <nav class="navbar navbar-expand-md tm-navbar" id="tmNav">
       <div class="container">
         <div class="tm-next">
-          <img src="../img/logo.png" width="60px">
+          <img src="../../public/img/logo.gif" width="60px">
           <a href="#" class="navbar-brand">E-LIFE</a>
         </div>
 
@@ -16,13 +16,13 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-              <a class="nav-link tm-nav-link" href="#introduction">Introduction</a>
+              <a class="nav-link tm-nav-link" href="#introduction">登录</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link tm-nav-link" href="#work">Our Work</a>
+              <a class="nav-link tm-nav-link" href="#work">注册</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link tm-nav-link" href="#contact">Contact Us</a>
+              <a class="nav-link tm-nav-link" href="#contact">联系我们</a>
             </li>
           </ul>
         </div>
@@ -31,10 +31,10 @@
 
     <div class="text-center tm-hero-text-container">
       <div class="tm-hero-text-container-inner">
-        <h2 class="tm-hero-title">The Town</h2>
+        <h2 class="tm-hero-title">E-LIFE</h2>
         <p class="tm-hero-subtitle">
-          Parallax Bootstrap Theme
-          <br />by TemplateMo
+          欢迎来到智能生活小区平台！
+          <br />
         </p>
       </div>
     </div>
@@ -50,22 +50,36 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-6">
-          <img src="../img/the-town-01.jpg" alt="Image" class="img-fluid tm-intro-img" />
+          <img src="../../public/img/intro.gif" alt="Image" class="img-fluid tm-intro-img" />
         </div>
         <div class="col-lg-6">
           <div class="tm-intro-text-container">
-            <h2 class="tm-text-primary mb-4 tm-section-title">Introduction</h2>
-            <p class="mb-4 tm-intro-text">
-              The Town is <strong>simple CSS template</strong> built on Bootstrap v4.1.3
-              and this is a little parallax layout that you can use for your websites.
-            </p>
-            <p class="mb-5 tm-intro-text">
-              Please tell your friends about our site
-              <a rel="nofollow" href="https://templatemo.com">templatemo</a>. Thank you.
-              Curabitur dapibus tristique enim a imperdiet. Etiam
-              tristique sem sed condimentum posuere. </p>
-            <div class="tm-next">
-              <a href="#work" class="tm-intro-text tm-btn-primary">Read More</a>
+            <h2 class="tm-text-primary mb-4 tm-section-title">在这里你可以做什么？</h2>
+            <el-tabs type="border-card" class="mb-4 tm-intro-text">
+              <el-tab-pane label="小区居民">
+                如果你是<strong>小区居民</strong>，你可以<br>
+                <li>享受小区服务和特有的优惠活动</li>
+                <li>第一时间获取最新资讯和紧急通知</li>
+                <li>在论坛上友好互动，增进邻里关系</li>
+              </el-tab-pane>
+              <el-tab-pane label="物业人员">
+                如果你是<strong>物业管理人员</strong>，你可以<br>
+                <li>更方便的发布通知和资讯</li>
+                <li>通知和收取管理费用</li>
+                <li>安排小区活动丰富居民生活</li>
+              </el-tab-pane>
+              <el-tab-pane label="周边商家">
+                如果你是<strong>商家、快递人员、超市人员</strong>，你可以<br>
+                <li>更方便的发布优惠信息</li>
+                <li>查看小区居民需求来调整优惠策略</li>
+                <li>收件派件更加快捷</li>
+              </el-tab-pane>
+            </el-tabs>
+            <h4>&nbsp;</h4>
+            <div class="tm-next" align="center">
+              <a href="#work" class="tm-intro-text tm-btn-primary">登录</a>
+              <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+              <a href="#work" class="tm-intro-text tm-btn-primary">注册</a>
             </div>
           </div>
         </div>
@@ -73,119 +87,81 @@
 
       <div class="row tm-section-pad-top">
         <div class="col-lg-4">
-          <i class="fas fa-4x fa-bus text-center tm-icon"></i>
-          <h4 class="text-center tm-text-primary mb-4">Curabitur at elit eu risus</h4>
+          <i class="fas fa-3x fa-address-book text-center tm-icon" style="float:left"></i>
+          <h3 class="text-center tm-text-primary mb-4" style="float:left">&nbsp;&nbsp;好友管理</h3>
+          <div style="clear:both"></div>
           <p>
-            Sed ultrices sit amet mi eu malesuada. Cras ultricies gravida
-            nisi, ac pellentesque nunc tincidunt quis. Aenean at ornare lacus.
-            Duis imperdiet lacus justo.
-          </p>
-        </div>
-
-        <div class="col-lg-4 mt-5 mt-lg-0">
-          <i class="fas fa-4x fa-bicycle text-center tm-icon"></i>
-          <h4 class="text-center tm-text-primary mb-4">Nunc sed metus vel nisi</h4>
-          <p>
-            Praesent ut finibus leo. Duis et tempus ipsum, id pretium nunc.
-            Vivamus imperdiet sem quis orci pharetra convallis. Nunc a tempus
-            nisi, sed fringilla purus. In hac habitasse platea.
+            用户可以注册登录智能生活小区平台，添加和管理好友
           </p>
         </div>
         <div class="col-lg-4 mt-5 mt-lg-0">
-          <i class="fas fa-4x fa-city text-center tm-icon"></i>
-          <h4 class="text-center tm-text-primary mb-4">Fusce sed nisi sagittis</h4>
+          <i class="fas fa-3x fa-bullhorn text-center tm-icon" style="float:left"></i>
+          <h3 class="text-center tm-text-primary mb-4" style="float:left">&nbsp;&nbsp;小区资讯</h3>
+          <div style="clear:both"></div>
           <p>
-            Donec vestibulum libero nisl. Curabitur ac orci ac lorem blandit
-            volutpat. Sed ac sodales nibh, ut porttitor elit. Sed id dui mi.
-            Vestibulum ante ipsum primis in faucibus.
+            在这里可以看到小区的公告、最新资讯、紧急通知
           </p>
         </div>
-      </div>
-    </div>
-  </section>
-
-  <section id="work" class="tm-section-pad-top">
-    <div class="container tm-container-gallery">
-      <div class="row">
-        <div class="text-center col-12">
-          <h2 class="tm-text-primary tm-section-title mb-4">Our Work</h2>
-          <p class="mx-auto tm-work-description">
-            "Curabitur ac orci ac lorem blandit volutpat. Sed ac sodales nibh, ut
-            porttitor elit. Sed id dui mi. Vestibulum ante ipsum primis in faucibus
-            orci luctus et ultrices posuere cubilia."
+        <div class="col-lg-4 mt-5 mt-lg-0">
+          <i class="fas fa-3x fa-bell text-center tm-icon" style="float:left"></i>
+          <h3 class="text-center tm-text-primary mb-4" style="float:left">&nbsp;&nbsp;物业通知</h3>
+          <div style="clear:both"></div>
+          <p>
+            物业管理人员会将通知发送给对应用户，用户可在通知栏查看
           </p>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-12">
-          <div class="mx-auto tm-gallery-container">
-            <div class="grid tm-gallery">
-              <a href="../img/gallery-img-01.jpg">
-                <figure class="effect-honey tm-gallery-item">
-                  <img src="../img/gallery-tn-01.jpg" alt="Image" class="img-fluid">
-                  <figcaption>
-                    <h2><i>Dreamy <span>Honey</span> Now</i></h2>
-                  </figcaption>
-                </figure>
-              </a>
-              <a href="../img/gallery-img-02.jpg">
-                <figure class="effect-honey tm-gallery-item">
-                  <img src="../img/gallery-tn-02.jpg" alt="Image" class="img-fluid">
-                  <figcaption>
-                    <h2><i>Second <span>Honey</span> Now</i></h2>
-                  </figcaption>
-                </figure>
-              </a>
-              <a href="../img/gallery-img-03.jpg">
-                <figure class="effect-honey tm-gallery-item">
-                  <img src="../img/gallery-tn-03.jpg" alt="Image" class="img-fluid">
-                  <figcaption>
-                    <h2><i>Third <span>Honey</span> Now</i></h2>
-                  </figcaption>
-                </figure>
-              </a>
-              <a href="../img/gallery-img-04.jpg">
-                <figure class="effect-honey tm-gallery-item">
-                  <img src="../img/gallery-tn-04.jpg" alt="Image" class="img-fluid">
-                  <figcaption>
-                    <h2><i>Dreamy <span>Honey</span> Now</i></h2>
-                  </figcaption>
-                </figure>
-              </a>
-              <a href="../img/gallery-img-05.jpg">
-                <figure class="effect-honey tm-gallery-item">
-                  <img src="../img/gallery-tn-05.jpg" alt="Image" class="img-fluid">
-                  <figcaption>
-                    <h2><i>Fifth <span>Honey</span> Now</i></h2>
-                  </figcaption>
-                </figure>
-              </a>
-              <a href="../img/gallery-img-06.jpg">
-                <figure class="effect-honey tm-gallery-item">
-                  <img src="../img/gallery-tn-06.jpg" alt="Image" class="img-fluid">
-                  <figcaption>
-                    <h2><i>Sixth <span>Honey</span> Now</i></h2>
-                  </figcaption>
-                </figure>
-              </a>
-              <a href="../img/gallery-img-01.jpg">
-                <figure class="effect-honey tm-gallery-item">
-                  <img src="../img/gallery-tn-01.jpg" alt="Image" class="img-fluid">
-                  <figcaption>
-                    <h2><i>Dreamy <span>Honey</span> Now</i></h2>
-                  </figcaption>
-                </figure>
-              </a>
-              <a href="../img/gallery-img-02.jpg">
-                <figure class="effect-honey tm-gallery-item">
-                  <img src="../img/gallery-tn-02.jpg" alt="Image" class="img-fluid">
-                  <figcaption>
-                    <h2><i>Eighth <span>Honey</span> Now</i></h2>
-                  </figcaption>
-                </figure>
-              </a>
-            </div>
-          </div>
+        <div class="col-lg-4">
+          <i class="fas fa-3x fa-envelope text-center tm-icon" style="float:left"></i>
+          <h3 class="text-center tm-text-primary mb-4" style="float:left">&nbsp;&nbsp;邮包提醒</h3>
+          <div style="clear:both"></div>
+          <p>
+            有邮包寄到小区被收发室代签之后，会给用户发送邮包提醒信息，
+            同时用户也可以享受上门寄件服务
+          </p>
+        </div>
+        <div class="col-lg-4 mt-5 mt-lg-0">
+          <i class="fas fa-3x fa-phone text-center tm-icon" style="float:left"></i>
+          <h3 class="text-center tm-text-primary mb-4" style="float:left">&nbsp;&nbsp;小区服务</h3>
+          <div style="clear:both"></div>
+          <p>
+            在这里用户可以享受超市送货、物业维修、电脑维修等服务
+          </p>
+        </div>
+        <div class="col-lg-4 mt-5 mt-lg-0">
+          <i class="fas fa-3x fa-shopping-cart text-center tm-icon" style="float:left"></i>
+          <h3 class="text-center tm-text-primary mb-4" style="float:left">&nbsp;&nbsp;小区团购</h3>
+          <div style="clear:both"></div>
+          <p>
+            是为小区居民特别推出的团购项目，小区居民只要展示手机客户端，
+            证明您是某小区的住户，在商家即可享受相应的团购优惠。
+            无需出示任何其他手续
+          </p>
+        </div>
+        <div class="col-lg-4">
+          <i class="fas fa-3x fa-comments text-center tm-icon" style="float:left"></i>
+          <h3 class="text-center tm-text-primary mb-4" style="float:left">&nbsp;&nbsp;小区话题</h3>
+          <div style="clear:both"></div>
+          <p>
+            平台上有小区论坛，用户可以在论坛中结识好友、畅所欲言
+          </p>
+        </div>
+        <div class="col-lg-4 mt-5 mt-lg-0">
+          <i class="fas fa-3x fa-hand-holding-usd text-center tm-icon" style="float:left"></i>
+          <h3 class="text-center tm-text-primary mb-4" style="float:left">&nbsp;&nbsp;周边优惠</h3>
+          <div style="clear:both"></div>
+          <p>
+            APP将以地图的方式，展示住户小区周边的优惠商家，团购活动等信息。
+            户主可以凭客户端，便捷的查找商家位置，
+            并在指定商家，享受专为住户提供的专项优惠活动，获得更多超值优惠。
+          </p>
+        </div>
+        <div class="col-lg-4 mt-5 mt-lg-0">
+          <i class="fas fa-3x fa-money-check text-center tm-icon" style="float:left"></i>
+          <h3 class="text-center tm-text-primary mb-4" style="float:left">&nbsp;&nbsp;物业缴费</h3>
+          <div style="clear:both"></div>
+          <p>
+            在线查询和缴纳物业费、停车费，无需前往物业收费处
+          </p>
         </div>
       </div>
     </div>
@@ -220,27 +196,8 @@
             <span class="mb-0">info@company.co</span>
           </a>
         </div>
-        <div class="col-sm-12 col-md-6 d-flex align-items-center tm-contact-item">
-          <a href="https://www.google.com/maps" class="tm-contact-item-link">
-            <i class="fas fa-3x fa-map-marker-alt mr-4"></i>
-            <span class="mb-0">Location on Maps</span>
-          </a>
-        </div>
-        <div class="col-sm-12 col-md-6 d-flex align-items-center tm-contact-item">
-          <form action="" method="get">
-            <input name="email" type="email" placeholder="Subscribe your email" class="tm-input" required />
-            <button type="submit" class="btn tm-btn-submit">Submit</button>
-          </form>
-        </div>
       </div>
     </div>
-    <footer class="text-center small tm-footer">
-      <p class="mb-0">
-        Copyright &copy; 2019 Company Name
-
-        - Design: <a rel="nofollow" href="https://templatemo.com" class="tm-footer-link">Template Mo</a>
-      </p>
-    </footer>
   </section>
   </body>
 </template>
@@ -248,3 +205,85 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 </style>
+
+<script>
+function getOffSet () {
+  var _offset = 450
+  var windowHeight = window.innerHeight
+
+  if (windowHeight > 500) {
+    _offset = 400
+  }
+  if (windowHeight > 680) {
+    _offset = 300
+  }
+  if (windowHeight > 830) {
+    _offset = 210
+  }
+
+  return _offset
+}
+
+function setParallaxPosition ($doc, multiplier, $object) {
+  let offset = getOffSet()
+  let fromTop = $doc.scrollTop()
+  let bgCss = 'center ' + (multiplier * fromTop - offset) + 'px'
+  $object.css({ 'background-position': bgCss })
+}
+
+// Parallax function
+// Adapted based on https://codepen.io/roborich/pen/wpAsm
+let backgroundImageParallax = function ($object, multiplier, forceSet) {
+  multiplier = typeof multiplier !== 'undefined' ? multiplier : 0.5
+  multiplier = 1 - multiplier
+  let $doc = $(document)
+  // $object.css({"background-attatchment" : "fixed"});
+
+  if (forceSet) {
+    setParallaxPosition($doc, multiplier, $object)
+  } else {
+    $(window).scroll(function () {
+      setParallaxPosition($doc, multiplier, $object)
+    })
+  }
+}
+
+$(function () {
+  // Hero Section - Background Parallax
+  backgroundImageParallax($('.tm-parallax'), 0.30, false)
+
+  // Handle window resize
+  window.addEventListener('resize', function () {
+    backgroundImageParallax($('.tm-parallax'), 0.30, true)
+  }, true)
+
+  // Detect window scroll and update navbar
+  $(window).scroll(function (e) {
+    if ($(document).scrollTop() > 120) {
+      $('.tm-navbar').addClass('scroll')
+    } else {
+      $('.tm-navbar').removeClass('scroll')
+    }
+  })
+
+  // Close mobile menu after click
+  $('#tmNav a').on('click', function () {
+    $('.navbar-collapse').removeClass('show')
+  })
+
+  // Add smooth scrolling to all links
+  // https://www.w3schools.com/howto/howto_css_smooth_scroll.asp
+  $('a').on('click', function (event) {
+    if (this.hash !== '') {
+      event.preventDefault()
+      let hash = this.hash
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 400, function () {
+        window.location.hash = hash
+      })
+    } // End if
+  })
+})
+</script>
