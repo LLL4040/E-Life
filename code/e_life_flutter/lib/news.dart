d6fcb59ffd4b521a49847b1230ff37c83d8cf30a
rzzhimo <1070634876@qq.com>
2019-07-17 17:42:32 +0800

    增加flutter

--- code/e_life_flutter/lib/news.dart
+++ code/e_life_flutter/lib/news.dart
@@ -1,78 +1,192 @@
 import 'package:flutter/material.dart';
 import 'bottom_navigation_widget.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'user.dart';
 
 class newsPage extends StatelessWidget {
   final Widget myDevider = Container(
     child: Divider(
-      color: Colors.black,
+      color: Colors.black12,
     ),
   );
-  Widget urgentSection = Container(
-    padding: const EdgeInsets.all(10),
-    child: Row(
+  Widget urgentSection = Expanded(
+    child: ListView(
       children: [
-        Expanded(
-          /*1*/
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              /*2*/
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Text(
-                  '紧急通知',
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Container(
-                child: Divider(),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: new Image.asset(
-                  'images/app.png',
-                  height: 200,
-                ),
-              ),
-            ],
+        Container(
+          padding: const EdgeInsets.only(bottom: 8),
+          child: Text(
+            '紧急通知',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+
+            ),
+            textAlign: TextAlign.center,
           ),
         ),
-        /*3*/
-//        new FavoriteWidget(),
+        Container(
+          child: Divider(),
+        ),
+        Container(
+          padding: EdgeInsets.all(10),
+          child: new Image.asset(
+            'images/app.png',
+            height: 100,
+          ),
+        ),
+
       ],
     ),
   );
-  Widget newsSection = new Padding(
-    padding: new EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 40.0),
+  Widget newsSection = new Expanded(
+    child: ListView(
+      children: <Widget>[
+        new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Text(
+                  "最新资讯",
+                  textAlign: TextAlign.center,
+                )
+              ],
+            ),
+          ],
+        ),
+        new ListTile(
+          leading: new Icon(Icons.nature_people),
+          title: new Text('活动1'),
+          subtitle: new Row(
+            children: <Widget>[
+              new Text('时间:2019/7/23'),
+              new Icon(Icons.person)
+            ],
+          ),
+          trailing: new Icon(Icons.keyboard_arrow_down),
+          onTap: () {
+            print('点击');
+          },
+          dense: true,
+        ),
+        new ListTile(
+          leading: new Icon(Icons.nature_people),
+          title: new Text('活动2'),
+          subtitle: new Row(
+            children: <Widget>[
+              new Text('时间:2019/7/29'),
+              new Icon(Icons.person)
+            ],
+          ),
+          trailing: new Icon(Icons.keyboard_arrow_down),
+          onTap: () {
+            print('点击');
+          },
+          dense: true,
+        ),
+        new ListTile(
+          leading: new Icon(Icons.nature_people),
+          title: new Text('活动3'),
+          subtitle: new Row(
+            children: <Widget>[new Text('时间:2019/7/9'), new Icon(Icons.person)],
+          ),
+          trailing: new Icon(Icons.keyboard_arrow_down),
+          onTap: () {
+            print('点击');
+          },
+          dense: true,
+        ),
+      ],
+    ),
   );
-  Widget activitySection = new Padding(
-    padding: new EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 40.0),
+  Widget activitySection = new Expanded(
+      child: ListView(
+        children: <Widget>[
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Text(
+                    "最新资讯",
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              ),
+            ],
+          ),
+          new ListTile(
+            leading: new Icon(Icons.nature_people),
+            title: new Text('活动1'),
+            subtitle: new Row(
+              children: <Widget>[
+                new Text('时间:2019/7/23'),
+                new Icon(Icons.person)
+              ],
+            ),
+            trailing: new Icon(Icons.keyboard_arrow_down),
+            onTap: () {
+              print('点击');
+            },
+            dense: true,
+          ),
+          new ListTile(
+            leading: new Icon(Icons.nature_people),
+            title: new Text('活动2'),
+            subtitle: new Row(
+              children: <Widget>[
+                new Text('时间:2019/7/29'),
+                new Icon(Icons.person)
+              ],
+            ),
+            trailing: new Icon(Icons.keyboard_arrow_down),
+            onTap: () {
+              print('点击');
+            },
+            dense: true,
+          ),
+          new ListTile(
+            leading: new Icon(Icons.nature_people),
+            title: new Text('活动3'),
+            subtitle: new Row(
+              children: <Widget>[new Text('时间:2019/7/9'), new Icon(Icons.person)],
+            ),
+            trailing: new Icon(Icons.keyboard_arrow_down),
+            onTap: () {
+              print('点击');
+            },
+            dense: true,
+          ),
+        ],
+      ),
   );
   @override
   Widget build(BuildContext context) {
+    void _toUser() {
+      Navigator.of(context)
+          .push(new MaterialPageRoute(builder: (context) => User()));
+    }
+
     return new Scaffold(
       appBar: new AppBar(
         actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.account_circle), onPressed: null),
+          new IconButton(
+              icon: new Icon(Icons.account_circle), onPressed: _toUser),
         ],
         title: new Text("资讯"),
       ),
-      body: new SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            urgentSection,
-            myDevider,
-            newsSection,
-            myDevider,
-            activitySection,
-            myDevider,
-            //new BottomNavigationWidget(),
-          ],
-        ),
+      body: Column(
+        children: <Widget>[
+          urgentSection,
+          myDevider,
+
+          newsSection,
+          myDevider,
+          activitySection,
+          myDevider,
+
+          //new BottomNavigationWidget(),
+        ],
       ),
     );
   }
