56f9de8e6370002b2410a2e93051c6bedab67d04
rzzhimo <1070634876@qq.com>
2019-09-01 19:38:10 +0800

    优化flutter论坛和参加活动

--- code/e_life_flutter/lib/service/pay.dart
+++ code/e_life_flutter/lib/service/pay.dart
@@ -2,35 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:e_life_flutter/service/addRepair.dart';
 import 'payhttp.dart';
 import 'package:tobias/tobias.dart' as tobias;
+
 class pay extends StatefulWidget {
   final username;
   var session;
-  pay(this.username,this.session);
+  pay(this.username, this.session);
   @override
   State<StatefulWidget> createState() {
-    return new payCenter(username,session);
+    return new payCenter(username, session);
   }
 }
 
-class payCenter extends State<pay> with SingleTickerProviderStateMixin,NetListener {
+class payCenter extends State<pay>
+    with SingleTickerProviderStateMixin, NetListener {
   final username;
   var session;
-  payCenter(this.username,this.session);
+  payCenter(this.username, this.session);
 
-  List<Bill> billList=[];//存后端数据
+  List<Bill> billList = []; //存后端数据
   payHttp manager = payHttp();
   Widget _getMyBill(var bill) {
     String repairStatus;
-    if (bill.status == -1||bill.status == -2) {
+    if (bill.status == -1 || bill.status == -2) {
       repairStatus = "未缴费";
-    }
-    else if (bill.status == 1||bill.status == 2) {
+    } else if (bill.status == 1 || bill.status == 2) {
       repairStatus = "已缴费";
-    }
-    else if (bill.status == -11||bill.status== -12) {
+    } else if (bill.status == -11 || bill.status == -12) {
       repairStatus = "支付中";
-    }else{
-      repairStatus ="其他";
+    } else {
+      repairStatus = "其他";
     }
 
     return new SizedBox(
@@ -43,37 +43,46 @@ class payCenter extends State<pay> with SingleTickerProviderStateMixin,NetListen
           // card只能有一个widget，但这个widget内容可以包含其他的widget
           children: [
             ListTile(
-              leading: bill.status==-1||bill.status==1||bill.status==11?new Icon(Icons.departure_board):new Icon(Icons.settings),
-              title: new Text(bill.bill.toString()),
-              subtitle: new Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  new Text(bill.time.toString()),
-    OutlineButton(child: Text("获取订单"), onPressed: () {
-    print(bill.id);
-    getOrderInfo(bill.id.toString(),bill.bill.toString(),bill.time.toString());
-    }),
-    OutlineButton(child: Text("支付"), onPressed: () {
-    print(":"+_payInfo+":1222");
-    doPayExec(_payInfo);
-    }),
-
-                ],
-              ),
-
-              trailing: new RaisedButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0)),
-                color: repairStatus=="已缴费"?Colors.blue[300]:repairStatus=="支付中"?Colors.orangeAccent[200]:Colors.redAccent[200],
-                textColor: Colors.white,
-                child: Text(repairStatus,
+                leading:
+                    bill.status == -1 || bill.status == 1 || bill.status == 11
+                        ? new Icon(Icons.departure_board)
+                        : new Icon(Icons.settings),
+                title: new Text(bill.bill.toString()),
+                subtitle: new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    new Text(bill.time.toString()),
+                    OutlineButton(
+                        child: Text("获取订单"),
+                        onPressed: () {
+                          print(bill.id);
+                          getOrderInfo(bill.id.toString(), bill.bill.toString(),
+                              bill.time.toString());
+                        }),
+                    OutlineButton(
+                        child: Text("支付"),
+                        onPressed: () {
+                          print(":" + _payInfo + ":1222");
+                          doPayExec(_payInfo);
+                        }),
+                  ],
                 ),
-                onPressed: () {
-                  // ...
-                },
-            )
-
-            )
+                trailing: new RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0)),
+                  color: repairStatus == "已缴费"
+                      ? Colors.blue[300]
+                      : repairStatus == "支付中"
+                          ? Colors.orangeAccent[200]
+                          : Colors.redAccent[200],
+                  textColor: Colors.white,
+                  child: Text(
+                    repairStatus,
+                  ),
+                  onPressed: () {
+                    // ...
+                  },
+                ))
           ],
         ),
       ),
@@ -81,7 +90,7 @@ class payCenter extends State<pay> with SingleTickerProviderStateMixin,NetListen
   }
 
   List<Widget> myMyBills = [];
-  var  _payInfo;
+  var _payInfo;
   Orders order;
   Map _payresult = {};
   String msg = "";
@@ -93,8 +102,8 @@ class payCenter extends State<pay> with SingleTickerProviderStateMixin,NetListen
   Widget build(BuildContext context) {
     myMyBills = [];
 
-    if(billList.length>0){
-      for(int i=0;i<billList.length-1;i++){
+    if (billList.length > 0) {
+      for (int i = 0; i < billList.length - 1; i++) {
         Widget mybill1 = _getMyBill(billList[i]);
         myMyBills.add(mybill1);
       }
@@ -118,23 +127,22 @@ class payCenter extends State<pay> with SingleTickerProviderStateMixin,NetListen
           )
         ],
       ),
-
     );
   }
-  void _returnMyBill()async{
-    await manager.myBill(this, username,session);
+
+  void _returnMyBill() async {
+    await manager.myBill(this, username, session);
   }
+
   void doPayExec(String apyInfo) async {
     Map payResult;
     try {
       payResult = await tobias.pay(_payInfo.toString());
-    }
-    on Exception catch (e) {
+    } on Exception catch (e) {
       payResult = {};
       print("hhh");
       return;
-    }
-    finally{
+    } finally {
       print("hhha");
     }
     if (!mounted) return;
@@ -142,46 +150,42 @@ class payCenter extends State<pay> with SingleTickerProviderStateMixin,NetListen
     setState(() {
       _payresult = payResult;
     });
-
   }
-  getOrderInfo(String id,String bill,String time) async {
+
+  getOrderInfo(String id, String bill, String time) async {
     print(id);
     await manager.myOrders(this, username, id, bill);
     await new Future.delayed(new Duration(milliseconds: 1000));
-    await manager.pay(this, order.id.toString(), order.bill.toString(), order.time);
-
+    await manager.pay(
+        this, order.id.toString(), order.bill.toString(), order.time);
   }
+
   @override
   void initState() {
-
     super.initState();
     _returnMyBill();
-
   }
-  @override
-  void onMyBillResponse(List<Bill> bill){
-    billList=bill;
-    setState(() {
 
-    });
-  }
   @override
-  void onOrderResponse(Orders myOrder){
+  void onMyBillResponse(List<Bill> bill) {
+    billList = bill;
+    setState(() {});
+  }
+
+  @override
+  void onOrderResponse(Orders myOrder) {
     order = myOrder;
-    setState(() {
-
-    });
+    setState(() {});
   }
+
   @override
-  void onAliResponse(String pay){
+  void onAliResponse(String pay) {
     _payInfo = pay;
-    setState(() {
-
-    });
-
+    setState(() {});
   }
+
   @override
-  void onError(error){
+  void onError(error) {
     print("$error");
   }
 }
