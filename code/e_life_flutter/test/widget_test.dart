5d4658c3c3b4483817352876560b7e3aee1006c4
rzzhimo <1070634876@qq.com>
2019-09-08 11:56:30 +0800

    增加flutter 更换小区，搜索优惠商品

--- code/e_life_flutter/test/widget_test.dart
+++ code/e_life_flutter/test/widget_test.dart
@@ -11,20 +11,29 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:e_life_flutter/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+//  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+//    // Build our app and trigger a frame.
+//    await tester.pumpWidget(MyApp());
+//
+//    // Verify that our counter starts at 0.
+//    expect(find.text('0'), findsOneWidget);
+//    expect(find.text('1'), findsNothing);
+//
+//    // Tap the '+' icon and trigger a frame.
+//    await tester.tap(find.byIcon(Icons.add));
+//    await tester.pump();
+//
+//    // Verify that our counter has incremented.
+//    expect(find.text('0'), findsNothing);
+//    expect(find.text('1'), findsOneWidget);
+//  });
+  test("String.split() splits the string on the delimiter", () {
+    var string = "foo,bar,baz";
+    expect(string.split(","), equals(["foo", "bar", "baz"]));
+  });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test("String.trim() removes surrounding whitespace", () {
+    var string = "  foo ";
+    expect(string.trim(), equals("foo"));
   });
 }
---
+++ code/elife/user-server/src/main/java/user/dao/UserDao.java
@@ -0,0 +1,78 @@
+package user.dao;
+
+import user.entity.User;
+
+import java.util.List;
+
+/**
+ * @author ztHou
+ */
+public interface UserDao {
+    /**
+     * save user
+     *
+     * @param user user
+     * @return user saved
+     */
+    User save(User user);
+
+    /**
+     * whether exists username in database
+     *
+     * @param username username
+     * @return whether exists username
+     */
+    Boolean existByUsername(String username);
+
+    /**
+     * whether exists phone in database
+     *
+     * @param phone phone
+     * @return whether exists phone
+     */
+    Boolean existsByPhone(String phone);
+
+    /**
+     * find user by username
+     *
+     * @param username username
+     * @return user found
+     */
+    User findByUsername(String username);
+
+    /**
+     * find user by phone
+     *
+     * @param phone phone
+     * @return user found
+     */
+    User findByPhone(String phone);
+
+    /**
+     * delete data from database by username
+     *
+     * @param username username
+     */
+    void deleteByUsername(String username);
+
+    /**
+     * find all user information contains the username
+     * @param username username
+     * @return all user that contains the username
+     */
+    List<User> findAllByUsernameContains(String username);
+
+    /**
+     * find all user in the given community
+     * @param communityId communityId
+     * @return all user in the given community
+     */
+    List<User> findAllByCommunityId(Long communityId);
+
+    /**
+     * find all username from community
+     * @param communityId community id
+     * @return all username
+     */
+    List<String> findUsernameByCommunityId(Long communityId);
+}
